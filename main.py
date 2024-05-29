1f43684817e29de79434b99c4f2662cb528cc41c
Ginanjar Adhitia <g.adhitia@gmail.com>
2024-05-29 19:24:07 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
